b60176a99a85ec290dc63a70f4203e31f5829611
Diego Barros <diegobarrosflf@gmail.com>
2020-06-15 19:23:09 -0300

    Criando o projeto

---
+++ src/test/java/com/poo/flf/PooProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.flf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
